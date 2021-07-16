acf26177dac27b5848f18ec6040632a4dfeb52c0
Redomic <redomicmc@gmail.com>
2021-07-16 12:50:20 +0530

    add projects page content and logic

---
+++ lib/models/projects.dart
@@ -0,0 +1,135 @@
+import 'package:flutter/material.dart';
+
+import '../constants/user_colors.dart';
+
+class LanguageUsage {
+  final String language;
+  final Color languageColor;
+  final int percentUsed;
+
+  LanguageUsage(
+    this.language,
+    this.languageColor,
+    this.percentUsed,
+  );
+}
+
+class UserProject {
+  final String title;
+  final String imageUrl;
+  final String description;
+  final bool isOpenSource;
+  final String repoLink;
+  final List<LanguageUsage> languagesUsed;
+  final bool isComplete;
+
+  UserProject({
+    required this.title,
+    required this.imageUrl,
+    required this.description,
+    this.isOpenSource = true,
+    required this.repoLink,
+    required this.languagesUsed,
+    required this.isComplete,
+  });
+}
+
+class UserProjects {
+  static List<UserProject> projects = [
+    // PORTFOLIO SITE - 0
+    UserProject(
+      title: 'Portfolio site',
+      imageUrl: 'https://i.imgur.com/LJKVEGN.png',
+      description: '''
+          This portfolio was made completely in Flutter
+          except for the background which was made with Vanta.js
+          ''',
+      repoLink: 'https://github.com/Redomic/redomic-portfolio',
+      languagesUsed: [
+        LanguageUsage('Dart', UserColors.flutterColor, 90),
+        LanguageUsage('HTML', UserColors.htmlColor, 10),
+      ],
+      isComplete: false,
+    ),
+    // CHAT APP - 1
+    UserProject(
+      title: 'Chat App',
+      imageUrl: 'https://i.imgur.com/7qKqqLf.png',
+      description: '''
+          A Simple Chat app made with Flutter to learn Firebase DB Integration
+          ''',
+      repoLink: 'https://github.com/Redomic/chat-app',
+      languagesUsed: [
+        LanguageUsage('Dart', UserColors.flutterColor, 100),
+      ],
+      isComplete: true,
+    ),
+    // GREAT PLACES APP - 2
+    UserProject(
+      title: 'Great Places App',
+      imageUrl: 'https://i.imgur.com/7qKqqLf.png',
+      description: '''
+          A Simple favorite places app made in Flutter to learn the Google Maps API
+          ''',
+      repoLink: 'https://github.com/Redomic/great-places-app',
+      languagesUsed: [
+        LanguageUsage('Dart', UserColors.flutterColor, 100),
+      ],
+      isComplete: false,
+    ),
+    // SHOP APP - 3
+    UserProject(
+      title: 'Shop App',
+      imageUrl: 'https://i.imgur.com/7qKqqLf.png',
+      description: '''
+          A Simple Shop interface app made in Flutter to learn about the Firebase Rest API
+          ''',
+      repoLink: 'https://github.com/Redomic/shop-app',
+      languagesUsed: [
+        LanguageUsage('Dart', UserColors.flutterColor, 100),
+      ],
+      isComplete: true,
+    ),
+    // Delimeals App - 4
+    UserProject(
+      title: 'Delimeals App',
+      imageUrl: 'https://i.imgur.com/7qKqqLf.png',
+      description: '''
+          A Simple favorite meals and recipes app made to learn Flutter
+          ''',
+      repoLink: 'https://github.com/Redomic/Delimeals-app',
+      languagesUsed: [
+        LanguageUsage('Dart', UserColors.flutterColor, 100),
+      ],
+      isComplete: true,
+    ),
+    // Personal Expenses App - 5
+    UserProject(
+      title: 'Expenses App',
+      imageUrl: 'https://i.imgur.com/7qKqqLf.png',
+      description: '''
+          A Simple personal expense tracker app made to learn Flutter
+          ''',
+      repoLink: 'https://github.com/Redomic/Personal-Expenses-app',
+      languagesUsed: [
+        LanguageUsage('Dart', UserColors.flutterColor, 100),
+      ],
+      isComplete: true,
+    ),
+    // Sitescrapper - 6
+    UserProject(
+      title: 'Sitescraper',
+      imageUrl: 'https://i.imgur.com/0iPQJh7.png',
+      description: '''
+          A Simple sitescraper python script that scrapes the news website
+          quantamagazine.org
+          It was made for a school project.
+          ''',
+      repoLink: 'https://github.com/Redomic/sitescraper_schoolproj',
+      languagesUsed: [
+        LanguageUsage('Python', UserColors.pythonColor, 100),
+      ],
+      isComplete: true,
+    ),
+  ];
+}
--- lib/pages/projects_page.dart
+++ lib/pages/projects_page.dart
@@ -1,6 +1,9 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 import '../constants/user_colors.dart';
+import '../models/projects.dart';
 
 class ProjectsPage extends StatelessWidget {
   final double screenWidth;
@@ -13,16 +16,125 @@ class ProjectsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final projects = UserProjects.projects;
+
     return Scaffold(
       backgroundColor: Colors.transparent,
       body: Center(
-        child: Text(
-          'UNDER WORK',
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 100,
-            fontWeight: FontWeight.bold,
-            fontStyle: FontStyle.italic,
+        child: Container(
+          width: 1000,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Projects',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontFamily: 'Montserrat',
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              SingleChildScrollView(
+                child: Container(
+                  height: 600,
+                  child: GridView.count(
+                    crossAxisCount: 4,
+                    children: projects
+                        .map(
+                          (element) => ProjectButtonHolder(
+                            title: element.title,
+                            imageUrl: element.imageUrl,
+                          ),
+                        )
+                        .toList(),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ProjectButtonHolder extends StatelessWidget {
+  final String title;
+  final String imageUrl;
+
+  ProjectButtonHolder({
+    required this.title,
+    required this.imageUrl,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final double containerHeight = 250;
+    final double containerWidth = 250;
+
+    return Container(
+      height: 250,
+      width: 250,
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: InkWell(
+          splashColor: Colors.transparent,
+          highlightColor: Colors.transparent,
+          onTap: () {},
+          child: Container(
+            decoration: BoxDecoration(
+              color: UserColors.navBarBorderColor,
+              borderRadius: BorderRadius.circular(30),
+              // border: Border.all(
+              //   width: 5,
+              //   color: UserColors.accentRedColor,
+              // ),
+            ),
+            height: containerHeight,
+            width: containerWidth,
+            child: Stack(
+              alignment: Alignment.center,
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(30),
+                  child: Container(
+                    height: containerHeight,
+                    width: containerHeight,
+                    child: Image.network(
+                      imageUrl,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(30),
+                  child: Align(
+                    alignment: Alignment.bottomCenter,
+                    child: Container(
+                      height: 50,
+                      width: containerWidth,
+                      color: Colors.black26,
+                    ),
+                  ),
+                ),
+                Positioned(
+                  bottom: 15,
+                  child: Text(
+                    title,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontFamily: 'Montserrat',
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
